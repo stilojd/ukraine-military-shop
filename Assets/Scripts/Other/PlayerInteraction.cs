6a53270c6abfdb947a224133146f65214f339c53
Karbovskyi Vladyslav <vkarbovskiy@qualium-systems.com>
2022-12-27 04:56:30 +0100

    environment-added

---
+++ Assets/Scripts/Other/PlayerInteraction.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    [SerializeField] private GameObject _store;
+    [SerializeField] private GameObject _inventory;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            _inventory.SetActive(!_inventory.gameObject.activeSelf);
+        }
+    }
+}
